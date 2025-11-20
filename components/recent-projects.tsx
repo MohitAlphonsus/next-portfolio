587c3ef15426ae8730e9cbae48fbd37349189334
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-11-20 12:08:05 +0530

    section of cards to display projects

---
+++ components/recent-projects.tsx
@@ -0,0 +1,78 @@
+import { projects } from "@/data";
+import { PinContainer } from "./ui/3dPin";
+import { ArrowUpRight } from "lucide-react";
+import Image from "next/image";
+
+export default function RecentProjects() {
+	return (
+		<div className="py-20" id="projects">
+			<h1 className="heading">
+				A small selection of{" "}
+				<span className="text-purple-300">recent projects</span>
+			</h1>
+			<div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+				{projects.map((item) => (
+					<div
+						className="lg:min-h-130 h-100 flex items-center justify-center sm:w-96 w-[80vw]"
+						key={item.id}
+					>
+						<PinContainer title="Visit" href={item.link}>
+							<div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[35vh] mb-10">
+								<div
+									className="relative w-full h-full overflow-hidden lg:rounded-3xl"
+									style={{ backgroundColor: "#13162D" }}
+								>
+									<Image src="/bg.png" alt="bgimg" fill />
+								</div>
+								<Image
+									src={item.img}
+									alt="cover"
+									className="z-10 absolute bottom-0 h-[90%] w-[90%]"
+									width={100}
+									height={100}
+								/>
+							</div>
+
+							<h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+								{item.title}
+							</h1>
+
+							<p
+								className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
+								style={{
+									color: "#BEC1DD",
+									margin: "1vh 0",
+								}}
+							>
+								{item.des}
+							</p>
+
+							<div className="flex items-center justify-between mt-7 mb-3">
+								<div className="flex items-center">
+									{item.iconLists.map((icon, index) => (
+										<div
+											key={index}
+											className="border border-white/20 bg-gray-950 rounded-full lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+											style={{
+												transform: `translateX(-${5 * index + 2}px)`,
+											}}
+										>
+											<Image src={icon} alt="icon5" className="p-2" fill />
+										</div>
+									))}
+								</div>
+
+								<div className="flex justify-center items-center">
+									<p className="flex lg:text-xl md:text-xs text-sm text-purple">
+										Check Live Site
+									</p>
+									<ArrowUpRight className="ms-3" color="#CBACF9" />
+								</div>
+							</div>
+						</PinContainer>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+}
