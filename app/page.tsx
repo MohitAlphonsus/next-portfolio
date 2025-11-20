f1b82787ab22186515acef2accd55ab2db59083b
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-11-20 13:53:42 +0530

    approach section added with accternity UI to display approach steps

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
+import Approach from "@/components/approach";
 import Grid from "@/components/grid";
 import Hero from "@/components/hero";
+import RecentProjects from "@/components/recent-projects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
 
 export default function Home() {
 	return (
 		<main className="flex flex-col justify-center items-center overflow-hidden bg-[#000319] text-white mx-auto px-5 sm:px-10">
 			<div className="max-w-7xl w-full">
-				<FloatingNav
-					navItems={[{ name: "About", link: "#about", icon: null }]}
-				/>
+				<FloatingNav navItems={navItems} />
 				<Hero />
 				<Grid />
+				<RecentProjects />
+				<Approach />
 			</div>
 		</main>
 	);
